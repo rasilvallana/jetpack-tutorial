2343d69d9585b4b0119616ce746f7c931e6679a9
Yco <rasilvallana@multisyscorp.com>
2022-01-04 12:56:53 +0800

    Setup jetpack compose
    Configured required libraries for jetpack. Also created my very first composable function.

--- app/src/main/java/com/rasilvallana/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/com/rasilvallana/jetpacktutorial/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.rasilvallana.jetpacktutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContent {
+            Text("Hello World!")
+        }
     }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("RAS")
 }
